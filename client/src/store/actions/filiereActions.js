2f0247fc1d0f95bc6cd805f636ffbb949e89153f
Hicham <Hicham@Hicham.KRAOU>
2020-12-22 15:53:50 +0100

    some error fixed mobile testing

--- client/src/store/actions/filiereActions.js
+++ client/src/store/actions/filiereActions.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 import { LOADING, DATA_RECEIVED, FETCHING_ERROR,UNAUTHORIZED_ERROR,
   FILIERE_ON_DELETION_PROCCESS, FILIERE_ADDING_ERROR,FILIERE_ON_ADDING_PROCCESS} from './TYPES';
 
-const token =localStorage.getItem("token")
-
 
 
 const getFilieres = () => async(dispatch) =>{
-  console.log('start fetching')
+  const token =localStorage.getItem("token")
 
   dispatch({
     type : LOADING 
@@ -16,24 +14,15 @@ const getFilieres = () => async(dispatch) =>{
 
   if(!token) return dispatch({type : UNAUTHORIZED_ERROR })
 
-   console.log('token is good : ',token)
-
   FetchData('api/admin/filieres',dispatch,token)
-  .then(data =>
-   { 
-      if (!data) {
-        console.log('data is not good :', data)
-        return
-      }
-      console.log('data is good :', data)
-      console.log('fetching finished')
-       return dispatch({
-          type : DATA_RECEIVED,
-          data
-        })
-   }
-  )
-  .catch((err)=> console.log('FetchData: ',err))
+  .then(data =>{ 
+    if (!data) return dispatch({type: FETCHING_ERROR})
+    return dispatch({
+      type : DATA_RECEIVED,
+      data
+    })
+  })
+  .catch(err => dispatch({type: FETCHING_ERROR}))
 }
 
 const FetchData = async(url,dispatch,token)=>{
@@ -48,7 +37,6 @@ const FetchData = async(url,dispatch,token)=>{
   .catch((err)=>{
     //handle UNAUTHORIZED_ERROR
     if (err.response.data.error) {
-      console.log(err.response.data.error)
        dispatch({type : UNAUTHORIZED_ERROR })
     }
   })
@@ -59,6 +47,8 @@ const editFiliere = id =>{
 }
 
 const deleteFiliere = id => async(dispatch) => {
+  const token =localStorage.getItem("token")
+
   if(!token) return dispatch({type : UNAUTHORIZED_ERROR })
   //delete filiere
   const headers = {
@@ -81,6 +71,8 @@ const deleteFiliere = id => async(dispatch) => {
 }
 
 const addFiliere = (abvname,filiere) => async(dispatch) =>{
+  const token =localStorage.getItem("token")
+
   if(!token) return dispatch({type : UNAUTHORIZED_ERROR })
   //set Authorization header
   const headers = {
